d762634989666fd5a3c9c88587b1228a3254c48f
Azzazy6269 <mohamadelazzazy@gmail.com>
2025-05-10 07:58:52 +0300

    Upload

---
+++ 00- README.txt
@@ -0,0 +1,26 @@
+
+This repository contains my practice implementations and problem-solving exercises on common data structures. Each folder focuses on a specific topic.
+
+## Topics Covered
+
+1. Stack
+2. Linked Stack
+3. Balanced Parentheses using Stack
+4. Postfix Problem using Stack
+5. Circular Queue
+6. Linked Queue
+7. Queue using Two Stacks
+8. ArrayList
+9. Linked List
+10. Doubly Linked List
+11. Binary Tree
+
+## Structure
+
+Each folder contains:
+- Implementation code
+- Example problems (if any)
+
+
+## Language Used
+C# (.NET)
---
+++ 12- Selection sort Algorithm/Program.cs
@@ -0,0 +1,53 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = new int[] { 2, 8, 7, 1, 5, 3, 4, 6 };
+        SelectionSort_Asc(arr, 8);
+        foreach (var i in arr)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+    static void SelectionSort_Asc(int[] arr,int n)
+    {
+        int minIndex;
+        for(int i = 0; i < n - 1; i++)
+        {
+            minIndex = i;
+            for (int j = i+1; j < n; j++)
+            {
+                if (arr[minIndex] > arr[j])
+                {
+                    minIndex = j;
+                }
+                   
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        
+    }
+    static void SelectionSort_Desc(int[] arr, int n)
+    {
+        int minIndex;
+        for (int i = 0; i < n - 1; i++)
+        {
+            minIndex = i;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[minIndex] > arr[j])
+                {
+                    minIndex = j;
+                }
+
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+
+    }
+}
