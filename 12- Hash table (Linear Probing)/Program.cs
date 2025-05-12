6bcbc7cedb07555f6ea883ef21688b5ba0ef7cd4
Azzazy6269 <mohamadelazzazy@gmail.com>
2025-05-12 21:18:53 +0300

    Upload hash table

---
+++ 12- Hash table (Linear Probing)/Program.cs
@@ -0,0 +1,106 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            var table = new HashTable(10);
+                table.Add(3);
+                table.Add(8);
+                table.Add(13);
+                table.Add(23);
+                table.Add(10);
+                table.Add(33);
+                table.Add(43);
+                table.Add(53);
+                table.Add(5);
+                table.Add(3);
+                table.Delete(3);
+
+            table.print();
+            Console.WriteLine(table.GetIndex(3));
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        
+    }
+}
+
+class HashTable
+{
+    Nullable<int>[] arr;
+    readonly int deleted = int.MinValue;
+    public HashTable(uint size)
+    {
+        arr = new Nullable<int>[size];
+    }
+
+    public void Add(int num) //Hashing
+    {
+        int count = 0;
+        int index = num % arr.Length;
+        while ( arr[index] != null && arr[index] != deleted)
+        {
+            if (index == arr.Length - 1)
+            {
+                index = 0;
+                count++;
+            }
+            else
+            {
+                index++;
+                count++;
+                if (count >= arr.Length)
+                    throw new Exception("Full table!!");
+            }
+                
+        }
+        arr[index] = num;
+    }
+    public void Delete(int num)
+    {
+        int index= GetIndex(num);
+        if (index == -1)
+            return;
+        arr[index] = deleted;
+    }
+    public int GetIndex(int num)
+    {
+        int count = 0;
+        int index = num % arr.Length;
+        while(count< arr.Length)
+        {
+            if (arr[index] == num)
+                return index;
+            else if(index == arr.Length - 1) 
+            { 
+                index = 0;
+                count++;
+            }
+            else
+            {
+                index++;
+                count++;
+            }
+                
+        }
+        return -1;
+        
+    }
+    public void print()
+    {
+        Console.WriteLine("--------");
+        foreach (var item in arr)
+        {
+            if (item == int.MinValue)
+                Console.WriteLine("");
+            else
+                Console.WriteLine(item);
+        }
+        Console.WriteLine("--------");
+
+    }
+}
---
+++ 13- Hash Table (Chaining)/Program.cs
@@ -0,0 +1,102 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            var table = new HashTable<int>(10);
+            table.Add(3);
+            table.Add(8);
+            table.Add(13);
+            table.Add(23);
+            table.Add(10);
+            table.Add(33);
+            table.Add(43);
+            table.Add(53);
+
+            table.Delete(33);
+            Console.WriteLine(table.Contains(33));
+            Console.WriteLine(table.Contains(10));
+            Console.WriteLine(table.Contains(3));
+            Console.WriteLine(table.Contains(23));
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+}
+
+class HashTable<T>
+{
+    Node[] arr;
+    class Node
+    {
+        public T item;
+        public Node next;
+    }
+    public HashTable(uint size)
+    {
+        arr = new Node[size];
+    }
+    public void Add(T element)
+    {
+        int index = Math.Abs(element.GetHashCode()) % arr.Length;
+        var newNode = new Node();
+        newNode.item = element;
+        if (arr[index] is null)
+        {
+            arr[index] = newNode;
+        }
+        else
+        {
+            Node temp = arr[index];
+            arr[index] = newNode;
+            newNode.next = temp;
+        }
+            
+    }
+    public void Delete(T element)
+    {
+        int index = Math.Abs(element.GetHashCode()) % arr.Length;
+        if (arr[index]!=null && arr[index].item.Equals(element))
+        {
+            arr[index] = arr[index].next;
+        }
+        else
+        {
+            Node previous = new Node() ;
+            Node temp = arr[index];
+            while (!temp.item.Equals(element))
+            {
+                
+                previous = temp;
+                temp = temp.next;
+                if (temp is null)
+                    throw new Exception("element isn't exist");
+            }
+            previous.next = temp.next;
+
+        }
+            
+    }
+    public bool Contains(T element)
+    {
+        int index = Math.Abs(element.GetHashCode()) % arr.Length;
+        if (arr[index] == null)
+            return false;
+        Node temp = arr[index];
+        while (true)
+        {
+            if (temp.item.Equals(element))
+                return true;
+            if (temp.next is null)
+                return false;
+            temp = temp.next;
+        }
+    }
+
+
+}
